fef50d1ed98b6c56724c753f3b3fdd747310afce
Ashwin kumar <ashwin.gps@gmail.com>
2020-06-19 14:45:17 +0530

    feat: 🎸 The exposed props can now be unexposed

--- src/components/inspector/panels/CustomComponentsPropsPanel.tsx
+++ src/components/inspector/panels/CustomComponentsPropsPanel.tsx
@@ -1,25 +1,41 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Input, Box } from '@chakra-ui/core'
+import { Input, Box, Flex } from '@chakra-ui/core'
 import FormControl from '../controls/FormControl'
 import { useForm } from '../../../hooks/useForm'
-import { getSelectedComponent } from '../../../core/selectors/components'
+import {
+  getSelectedComponent,
+  getShowCustomComponentPage,
+} from '../../../core/selectors/components'
+import useDispatch from '../../../hooks/useDispatch'
+import ActionButton from '../ActionButton'
 
 const CustomComponentsPropsPanel = () => {
   const { setValueFromEvent } = useForm()
   const selectedComponent = useSelector(getSelectedComponent)
+  const dispatch = useDispatch()
+  const isCustomComponentPage = useSelector(getShowCustomComponentPage)
 
   return (
     <>
       {Object.keys(selectedComponent.props).map(prop => (
         <Box key={prop} m="10px">
           <FormControl label={prop} htmlFor={prop}>
-            <Input
-              value={selectedComponent.props[prop]}
-              size="sm"
-              name={prop}
-              onChange={setValueFromEvent}
-            />
+            <Flex alignItems="center">
+              <Input
+                value={selectedComponent.props[prop]}
+                size="sm"
+                name={prop}
+                onChange={setValueFromEvent}
+              />
+              {isCustomComponentPage ? (
+                <ActionButton
+                  label="delete Exposed prop"
+                  icon="small-close"
+                  onClick={() => dispatch.components.deleteExposedProp(prop)}
+                />
+              ) : null}
+            </Flex>
           </FormControl>
         </Box>
       ))}
