d50b7711c356f727747bc5929f4ae35061d2dc26
Ashwin kumar <ashwin.gps@gmail.com>
2020-10-16 20:04:45 +0530

    fix: 🐛 The text gets disappeared when the input is blurred
    When the text input is focused and then clicked outside, the text on the
    component gets disappeared.

--- src/components/inspector/controls/ChildrenControl.tsx
+++ src/components/inspector/controls/ChildrenControl.tsx
@@ -1,12 +1,10 @@
 import React, { useRef, KeyboardEvent } from 'react'
 import { Input } from '@chakra-ui/core'
 import FormControl from './FormControl'
-import useDispatch from '../../../hooks/useDispatch'
 import { useForm } from '../../../hooks/useForm'
 import usePropsSelector from '../../../hooks/usePropsSelector'
 
 const ChildrenControl: React.FC = () => {
-  const dispatch = useDispatch()
   const textInput = useRef<HTMLInputElement>(null)
   const { setValueFromEvent } = useForm()
   const children = usePropsSelector('children')
@@ -27,9 +25,6 @@ const ChildrenControl: React.FC = () => {
         onChange={setValueFromEvent}
         ref={textInput}
         onKeyUp={onKeyUp}
-        onBlur={() => {
-          dispatch.app.toggleInputText(false)
-        }}
       />
     </FormControl>
   )
