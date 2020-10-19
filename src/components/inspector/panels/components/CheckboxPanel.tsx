5f35e5da01b9f430d9cd8c4c5dfe5a75587b4f03
Ashwin kumar <ashwin.gps@gmail.com>
2020-10-19 12:56:29 +0530

    Removed multiple labels for the size control in check-box

--- src/components/inspector/panels/components/CheckboxPanel.tsx
+++ src/components/inspector/panels/components/CheckboxPanel.tsx
@@ -1,7 +1,6 @@
 import React, { memo } from 'react'
 import ColorsControl from '../../controls/ColorsControl'
 import ChildrenControl from '../../controls/ChildrenControl'
-import FormControl from '../../controls/FormControl'
 import usePropsSelector from '../../../../hooks/usePropsSelector'
 import SwitchControl from '../../controls/SwitchControl'
 import SizeControl from '../../controls/SizeControl'
@@ -14,9 +13,7 @@ const CheckboxPanel = () => {
       <ChildrenControl />
       <SwitchControl label="Checked" name="defaultIsChecked" />
       <ColorsControl label="Color Scheme" name="colorScheme" />
-      <FormControl label="Size" htmlFor="size">
-        <SizeControl value={size} options={['sm', 'md', 'lg']} />
-      </FormControl>
+      <SizeControl value={size} options={['sm', 'md', 'lg']} />
     </>
   )
 }
