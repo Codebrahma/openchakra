7d85d7791282f34074a5bd6a85e7a860e3055249
Ashwin kumar <ashwin.gps@gmail.com>
2020-12-08 12:15:15 +0530

    Handling of empty value in prop

--- src/babel-plugins/set-prop-plugin.ts
+++ src/babel-plugins/set-prop-plugin.ts
@@ -15,16 +15,22 @@ const setPropPlugin = (
       JSXOpeningElement(path: any) {
         const visitedComponentId = getComponentId(path.node)
 
-        if (visitedComponentId === componentId) {
-          const jsxAttribute = toJsxAttribute(propName, value)
-          const existingAttrIndex = path.node.attributes.findIndex(
-            (node: any) => node.name.name === propName,
-          )
-          if (existingAttrIndex !== -1) {
+        if (visitedComponentId !== componentId) return
+
+        const jsxAttribute = toJsxAttribute(propName, value)
+        const existingAttrIndex = path.node.attributes.findIndex(
+          (node: any) => node.name.name === propName,
+        )
+        if (existingAttrIndex !== -1) {
+          if (value.length > 0) {
             path.node.attributes[existingAttrIndex] = jsxAttribute
           } else {
-            path.node.attributes.push(jsxAttribute)
+            // If the value is empty, remove the prop.
+            path.node.attributes.splice(existingAttrIndex, 1)
           }
+        } else {
+          // Push the attribute only when the value is not empty.
+          value.length > 0 && path.node.attributes.push(jsxAttribute)
         }
       },
     },
