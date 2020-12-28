ac6c4f8e741d6029daff5a10e419c60b71ec0452
Ashwin kumar <ashwin.gps@gmail.com>
2020-12-28 14:31:06 +0530

    Handled moving of components.

---
+++ src/babel-plugins/move-component-plugin/remove-component.ts
@@ -0,0 +1,31 @@
+import { getComponentId } from '../utils/babel-plugin-utils'
+import { declare } from '@babel/helper-plugin-utils'
+
+class removeComponentPlugin {
+  removedComponent: string = ``
+  plugin: any
+
+  constructor(options: { componentId: string }) {
+    const { componentId } = options
+
+    this.plugin = declare(() => {
+      return {
+        visitor: {
+          JSXElement: (path: any) => {
+            const element = path.node
+            const visitedComponentId = getComponentId(element.openingElement)
+            if (visitedComponentId === componentId) {
+              // Remove the element from its original position
+              console.log(path.toString())
+              this.removedComponent = path.toString()
+              path.remove()
+              path.stop()
+            }
+          },
+        },
+      }
+    })
+  }
+}
+
+export default removeComponentPlugin
