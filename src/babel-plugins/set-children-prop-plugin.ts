a608a100331002dcd9ff63f16b45271d7f02acc4
Ashwin kumar <ashwin.gps@gmail.com>
2021-01-08 14:51:21 +0530

    plugin created for handling the span elements

---
+++ src/babel-plugins/set-children-prop-plugin.ts
@@ -0,0 +1,41 @@
+import { getComponentId, toJsxText, getNode } from './utils/babel-plugin-utils'
+
+export type ISpanComponentsValues = { [spanComponentId: string]: string }
+
+const setPropPlugin = (
+  _: any,
+  options: {
+    componentId: string
+    value: string[]
+    spanComponentsValues: ISpanComponentsValues
+  },
+) => {
+  const { componentId, value, spanComponentsValues } = options
+
+  return {
+    visitor: {
+      JSXElement(path: any) {
+        const openingElement = path.node.openingElement
+        const visitedComponentId = getComponentId(openingElement)
+
+        if (visitedComponentId !== componentId) return
+
+        const children: any = []
+
+        // If it is span element, create the span component and push it to the children array.
+        // or else just push the value to the children array.
+        value.forEach(val => {
+          if (spanComponentsValues[val]) {
+            const element = `<Box as="span" compId="${val}">${spanComponentsValues[val]}</Box>`
+            children.push(getNode(element).expression)
+          } else {
+            children.push(toJsxText(val))
+          }
+        })
+        path.node.children = children
+      },
+    },
+  }
+}
+
+export default setPropPlugin
--- src/components/actionButtons/SpanActionButton.tsx
+++ src/components/actionButtons/SpanActionButton.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { IoMdBrush } from 'react-icons/io'
 import { useToast } from '@chakra-ui/core'
@@ -10,13 +11,33 @@ import {
   getSelectedTextDetails,
   getIsSelectionEnabled,
 } from '../../core/selectors/text'
-import { isSelectedRangeContainsTwoSpan } from '../../core/selectors/components'
+import {
+  isSelectedRangeContainsTwoSpan,
+  getSelectedComponentId,
+  getPropsBy,
+  getProps,
+  getSelectedPage,
+  getCustomComponents,
+} from '../../core/selectors/components'
+import { getCode, getAllComponentsCode } from '../../core/selectors/code'
+import babelQueries from '../../babel-queries/queries'
+import { ISpanComponentsValues } from '../../babel-plugins/set-children-prop-plugin'
+import { searchRootCustomComponent } from '../../utils/recursive'
 
 const SpanActionButton = () => {
   const toast = useToast()
   const dispatch = useDispatch()
   const containsOnlySpan = useSelector(getIsContainsOnlySpan)
   const selectedTextDetails = useSelector(getSelectedTextDetails)
+  const selectedComponentId = useSelector(getSelectedComponentId)
+  const prop = useSelector(getPropsBy(selectedComponentId))
+  const props = useSelector(getProps(selectedComponentId))
+  const code = useSelector(getCode)
+  const selectedPage = useSelector(getSelectedPage)
+  const childrenProp = prop.find(prop => prop.name === 'children')
+  const customComponents = useSelector(getCustomComponents)
+  const isChildOfCustomComponent = customComponents[selectedComponentId]
+  const componentsCode = useSelector(getAllComponentsCode)
 
   const isSelectedTwoSpan = useSelector(
     isSelectedRangeContainsTwoSpan({
@@ -25,6 +46,54 @@ const SpanActionButton = () => {
     }),
   )
   const isSelectionEnabled = useSelector(getIsSelectionEnabled)
+
+  useEffect(() => {
+    if (childrenProp) {
+      setTimeout(() => {
+        const { value } = childrenProp
+
+        // span component-id will be the key and its children prop value will be the value
+        const spanComponentsValues: ISpanComponentsValues = {}
+        if (value.length > 0 && Array.isArray(value)) {
+          value.forEach((val: string) => {
+            if (props.byComponentId[val]) {
+              props.byComponentId[val].forEach(propId => {
+                if (props.byId[propId].name === 'children') {
+                  spanComponentsValues[val] = props.byId[propId].value
+                }
+              })
+            }
+          })
+
+          if (isChildOfCustomComponent) {
+            const rootCustomParentElement = searchRootCustomComponent(
+              customComponents[selectedComponentId],
+              customComponents,
+            )
+            const updatedCode = babelQueries.setChildrenProp(
+              componentsCode[rootCustomParentElement],
+              {
+                componentId: selectedComponentId,
+                value,
+                spanComponentsValues,
+              },
+            )
+            dispatch.code.setComponentsCode(
+              updatedCode,
+              rootCustomParentElement,
+            )
+          } else {
+            const updatedCode = babelQueries.setChildrenProp(code, {
+              componentId: selectedComponentId,
+              value,
+              spanComponentsValues,
+            })
+            dispatch.code.setPageCode(updatedCode, selectedPage)
+          }
+        }
+      }, 500)
+    }
+  }, [childrenProp])
 
   const clickHandler = () => {
     if (isSelectedTwoSpan) {
