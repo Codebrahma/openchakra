84ddc696157cb9cdc025b0fd1e658c6b8cf91e71
Ashwin kumar <ashwin.gps@gmail.com>
2021-02-10 09:01:42 +0530

    Removed unneccesary javascript files

--- src/components/actionButtons/CodeActionButton.tsx
+++ src/components/actionButtons/CodeActionButton.tsx
@@ -1,40 +1,58 @@
 import React, { useState, memo } from 'react'
 import { useSelector } from 'react-redux'
-
-import {
-  getSelectedComponent,
-  getProps,
-  getComponents,
-} from '../../core/selectors/components'
-import { generateComponentCode } from '../../utils/codeGeneration/code'
-import useClipboard from '../../hooks/useClipboard'
-import ActionButton from './ActionButton'
 import { CheckIcon } from '@chakra-ui/icons'
 import { GoCode } from 'react-icons/go'
+
+import {
+  getSelectedComponentId,
+  isChildrenOfCustomComponent,
+  getCustomComponents,
+} from '../../core/selectors/components'
+import useClipboard from '../../hooks/useClipboard'
+import ActionButton from './ActionButton'
+import babelQueries from '../../babel-queries/queries'
+import { getCode, getAllComponentsCode } from '../../core/selectors/code'
+import { searchRootCustomComponent } from '../../utils/recursive'
 
 const CodeActionButton = memo(() => {
   const [isLoading, setIsLoading] = useState(false)
   const { onCopy, hasCopied } = useClipboard()
-  const selectedComponent = useSelector(getSelectedComponent)
-  const parentId = selectedComponent.parent
-  const components = useSelector(getComponents(parentId))
-  const props = useSelector(getProps(parentId))
+  const componentId = useSelector(getSelectedComponentId)
+  const customComponents = useSelector(getCustomComponents)
+  const pageCode = useSelector(getCode)
+  const isCustomComponentChild = useSelector(
+    isChildrenOfCustomComponent(componentId),
+  )
+  const componentsCode = useSelector(getAllComponentsCode)
+
+  let rootParentOfParentElement: string = ``
+
+  if (isCustomComponentChild) {
+    rootParentOfParentElement = searchRootCustomComponent(
+      customComponents[componentId],
+      customComponents,
+    )
+  }
+  const code = isCustomComponentChild
+    ? componentsCode[rootParentOfParentElement]
+    : pageCode
+
+  const clickHandler = () => {
+    setIsLoading(true)
+    const componentCode = babelQueries.getComponent(code, { componentId })
+    const componentCodeWithoutCompId = babelQueries.removeComponentId(
+      componentCode,
+    )
+    onCopy(componentCodeWithoutCompId)
+    setIsLoading(false)
+  }
 
   return (
     <ActionButton
       isLoading={isLoading}
       label="Copy code component"
       colorScheme={hasCopied ? 'green' : 'gray'}
-      onClick={async () => {
-        setIsLoading(true)
-        const code = await generateComponentCode(
-          { ...components[parentId], children: [selectedComponent.id] },
-          components,
-          props,
-        )
-        onCopy(code)
-        setIsLoading(false)
-      }}
+      onClick={clickHandler}
       icon={hasCopied ? <CheckIcon /> : <GoCode />}
     />
   )
